2ae082574d51c5c2afcca4933223f7314b397f1a
Arhaan Gupta-Rastogi <119004196+Arhaan23421@users.noreply.github.com>
2022-11-25 19:59:08 +0000

    we created differences and created the data for the athletes.

---
+++ classes.py
@@ -0,0 +1,31 @@
+class Athlete:
+  name = ""
+  pushups = []
+  pullups = []
+  maxhang = []
+  maxplank = []
+  squats = []
+  top5 = []
+
+  def getPushupDifference(self):
+    return self.pushups[1]-self.pushups[0]
+    pass
+  def getPullupDifference(self):
+    return self.pullups[1]- self.pullups[0]
+    pass
+  def getSquatDifference(self):
+    return self.squats[1]- self.squats[0]
+    pass
+
+
+  def getMaxHangDifference(self):
+    pass
+  def getMaxPlankDifference(self):
+    x-y
+    pass
+  def getTop5Difference(self):
+    x-y
+    pass
+  def getTop5Average(self):
+    x-y
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,70 @@
-print("hi")
+import csv
+from classes import Athlete
+
+f = open("data.csv" , 'r')
+
+greg = Athlete()
+greg.name = "Greg"
+greg.pushups.append(31)
+greg.pushups.append(41)
+greg.pullups.append(31)
+greg.pullups.append(41)
+#for time, left of : times 60 + right of :
+#'1:33' -> 1,33, (1x60) + 33
+greg.maxhang.append(470)
+greg.maxhang.append(490)
+greg.maxplank.append(500)
+greg.maxplank.append(501)
+greg.squats.append(90)
+greg.squats.append(100)
+#"4 4 4 4 4" -> ['4','4','4',4,4] 
+greg.top5.append([4,4,4,4,4])
+greg.top5.append([5,4,5,2,3])
+
+
+athletes = []
+with f:
+  reader = csv.reader(f)
+  skip = next(reader)
+  while(True):
+    try:
+      row1 = next(reader)
+      row2 = next(reader)
+    except:
+      break
+      
+    
+    a = Athlete()
+    a.name = row1[0]
+    if(len(row1[1]) > 0 and row1[1] != "injury"):
+      a.pushups = [int(row1[1])]
+    else:
+      a.pushups = [0]
+      
+    if(len(row2[1]) > 0 and row2[1] != "injury"):
+      a.pushups.append(int(row2[1]))
+    else:
+      a.pushups.append(0)
+      
+    if(len(row1[2]) > 0) and row1[2] != "injury":
+      a.pullups = [int(row1[1])]
+    else:
+      a.pullups= [0]
+    if(len(row2[2]) > 0 and row2[2] != "injury"):
+      a.pullups.append(int(row2[2]))
+    else:
+      a.pullups.append(0)
+    a.maxhang = [row1[3] , row2[3]]
+    a.maxplank = [row1[4] , row2[4]]
+    if(len(row1[5]) > 0 and row1[5] != "injury"):
+      a.squats = [int(row1[5])]
+    else:
+      a.squats= [0]
+    if(len(row2[5]) > 0 and row2[5] != "injury"):
+      a.squats.append(int(row2[5]))
+    else:
+      a.squats.append(0)
+    a.top5 = [row1[7] , row2[7]]
+    athletes.append(a)
+for a in athletes:
+  print(a.name, a.getPushupDifference(), a.getSquatDifference(), a.getPullupDifference())
